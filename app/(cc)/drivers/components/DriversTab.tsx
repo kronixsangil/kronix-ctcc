d084221ba9c659e26bb3dc26fe42ca73f188ee57
kronixsangil <blassena@hotmail.com>
2026-09-12 10:04:07 -0500

    photo Driver

--- app/(cc)/drivers/components/DriversTab.tsx
+++ app/(cc)/drivers/components/DriversTab.tsx
@@ -979,9 +979,9 @@ function toggleSelectedWorkerType(value: string) {
                     setDriversLimit(Number(e.target.value));
                   }}
                 >
-                  <option value={10}>10</option>
-                  <option value={20}>20</option>
                   <option value={30}>30</option>
+                  <option value={50}>50</option>
+                  <option value={100}>100</option>
                 </select>
               </div>
 
